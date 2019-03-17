2fca357cd5edc82b125d3c9d23826b388ec81ed8
Douglas Ciarfella <losdougie@comcast.net>
2019-03-17 14:05:03 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "You have reached the index."
+
+
+@app.route("/file/<file>")
+def show_text_file(file):
+    title = "ToDo"
+    filename = file + ".txt"
+    file_text = []
+    if not os.path.isfile(filename):
+        return render_template(
+            "todo_list.html", title="File Not Found", list_data=["Cannot find file."]
+        )
+    with open(filename, "r") as f:
+        for line in f:
+            file_text.append(line.strip())
+    return render_template("todo_list.html", title=title, list_data=file_text)
+
+
+@app.route("/temp")
+def show_template():
+    title = "Template"
+    test = "Test"
+    return render_template("todo_list.html", title=title, list_data=test)
