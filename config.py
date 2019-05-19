faee064977172f5b90cdc3fa6594d8657afa8297
Douglas Ciarfella <losdougie@comcast.net>
2019-05-19 12:13:14 -0400

    config example used

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
+import custom_defs
 
 def read_config(file):
     default_settings = {
         "scanner": "firefly",
         "fwhm": "0.2",
         "sectral_sl": "0.05",
-        "spacial_sl": "0.05"
+        "spacial_sl": "0.05",
+        "custom_fwhm": False
     }
     if not os.path.isfile(file):
         print("Settings file cannot be found.")
@@ -15,7 +17,13 @@ def read_config(file):
         for line in f:
             setting_data = line.split("=", 1)
             setting = setting_data[0].lower().strip()
-            data = setting_data[1].strip()
+            data = setting_data[1]
+            if data.lower() == "true":
+                data = True
+            elif data.lower() == "false":
+                data = False
+            else:
+                data.strip()
             loaded_settings[setting] = data
     for setting in loaded_settings:
         if setting not in default_settings:
@@ -24,11 +32,50 @@ def read_config(file):
             default_settings[setting] = loaded_settings[setting]
     return default_settings
 
+def fwhm(spacial, spectral, function_type=None):
+    fwhm_defs = {
+        "default": fwhm,
+        "option_2": fwhm2,
+        "custom": custom_defs.fwhm
+    }
+    # if function_type is not None:
+    #     return fwhm_defs[function_type](spacial, spectral)
+    # else:
+    #     return fwhm_default(spacial, spectral)
+    if settings["custom_fwhm"]:
+        return fwhm_defs["custom"](spacial, spectral)
+    else:
+        return fwhm_default(spacial, spectral)
+    # print(fwhm_defs["default"](1, 2))
+    # print(fwhm_defs["option_2"](1, 2))
+    # print(fwhm_defs["custom"](1, 2))
+
+def fwhm_default(spacial, spectral):
+    return spacial*2 + spectral*100
+
+def fwhm2(spacial, spectral):
+    return spacial*1000 + spectral
 
 def main():
+    global settings
     settings = read_config("settings.conf")
     for setting in settings:
         print("{} is set to {}".format(setting, settings[setting]))
+    # if settings["custom_fwhm"]:
+    #     print("ok")
+    #     fwhm(1, 2, function_type="custom")
+    # else:
+    #     fwhm(1, 2)
+    # fwhm_defs = {
+    #     "default": fwhm,
+    #     "option_2": fwhm2,
+    #     "custom": custom_defs.fwhm
+    # }
+    #
+    # print(fwhm_defs["default"](1, 2))
+    # print(fwhm_defs["option_2"](1, 2))
+    # print(fwhm_defs["custom"](1, 2))
+    print(fwhm(1, 2))
 
 if __name__ == "__main__":
     main()
---
+++ custom_defs.py
@@ -0,0 +1,3 @@
+def fwhm(spacial, spectral):
+    # custom fucnction can be used here. set settings.conf "custom_fwhm" to True
+    pass
